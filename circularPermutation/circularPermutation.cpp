40795fc460543f41682d0cb9b5daa16711c6bdb5
Heathcliff <2826190541@qq.com>
2023-02-24 23:24:35 +0800

    update

---
+++ circularPermutation/circularPermutation.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> circularPermutation(int n, int start) {
+
+    }
+};
+
+int main()
+{
+    int n = 2, start = 3;
+    vector<int>nums = Solution().circularPermutation(n, start);
+    for (int i = 0; i < nums.size(); i++)
+        cout << nums[i] << " ";
+}
---
+++ maxProfit0/maxProfit0.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int res = 0, buy = 0x7fffffff, sell = 0;
+        for (vector<int>::iterator it = prices.begin(); it != prices.end(); it++) {
+            buy = buy < *it ? buy : *it;
+            res = res < *it - buy ? *it - buy : res;
+        }
+        return res;
+    }
+};
+
+int main()
+{
+    vector<int>prices;
+    int nums[] = { 7,6,4,3,1 };
+    for (int i = 0; i < 6; i++)
+        prices.push_back(nums[i]);
+    cout << Solution().maxProfit(prices) << endl;
+}
