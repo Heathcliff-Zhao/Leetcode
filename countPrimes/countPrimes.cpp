2a8fbab43c4b3610604a2cf71220de6ac3fca39f
Heathcliff <2826190541@qq.com>
2023-03-02 15:36:27 +0800

    update

---
+++ countPrimes/countPrimes.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int countPrimes(int n) {
+        vector<bool>arr(n);
+        int cnt = 0;
+        for (int i = 2; i < n; i++) {
+            if (arr[i])
+                continue;
+            cnt++;
+            for (int j = i; j < n; j += i)
+                arr[j] = true;
+        }
+        return cnt;
+    }
+};
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << Solution().countPrimes(n) << endl;
+}
---
+++ maxArea/maxArea.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left = 0, right = height.size() - 1;
+        int ans = 0;
+        while (left < right) {
+            int area = min(height[left], height[right]) * (right - left);
+            ans = max(ans, area);
+            if (height[left] <= height[right])
+                left++;
+            else
+                right--;
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    vector<int>height;
+    int nums[] = { 1,8,6,2,5,4,8,3,7 };
+    for (int i = 0; i < sizeof(nums) / 4; i++)
+        height.push_back(nums[i]);
+    cout << Solution().maxArea(height) << endl;
+}
